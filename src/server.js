b1c7174e52f1dc00cacfc9726e9245eb143345f4
Pedro <pedrof.salim@gmail.com>
2020-06-05 21:46:07 -0300

    src

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const server = express()
 
+const db = require("./database/db")
 
 const nunjucks = require('nunjucks')
 nunjucks.configure("src/views", {
@@ -11,17 +12,83 @@ nunjucks.configure("src/views", {
 
 server.use(express.static('public'))
 
+server.use(express.urlencoded({ extended: true }))
+
 
 server.get('/', function(req, res) {
     return res.render("index.html", { title: "Um título"})
 })
 
 server.get('/create-point', function(req, res) {
+
+    
+
+
     return res.render("create-point.html")
 })
 
+server.post("/savepoint", (req, res) => {
+
+
+    const query = `
+        INSERT INTO places (
+            image,
+            name,
+            address,
+            address2,
+            state,
+            city,
+            items
+        ) VALUES (?,?,?,?,?,?,?);
+    `
+
+    const values = [
+        req.body.image,
+        req.body.name,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ]
+
+    function afterInsertData(err) {
+        if(err) {
+            console.log(err)
+            return res.send("Erro no cadastro!")
+        }
+
+        console.log("Cadastrado com sucesso")
+        console.log(this)
+
+        return res.render("create-point.html", {saved: true})
+    }
+
+    db.run(query, values, afterInsertData)
+
+})
+
+
+
 server.get('/search', function(req, res) {
-    return res.render("search-results.html")
+
+    const search = req.query.search
+
+    if(search == "") {
+        return res.render("search-results.html", { total: 0 })
+    }
+
+
+    db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows) {
+        if(err) {
+            return console.log(err)
+        }
+
+        const total = rows.length
+
+        return res.render("search-results.html", { places: rows, total: total })
+    })
+    
 })
 
 server.listen(3000, function() {
